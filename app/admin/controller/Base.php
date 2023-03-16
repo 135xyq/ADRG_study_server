95ab4bb2f485b1d478ac59e170b57a4d68e03a9e
xyq <xyq135@gmail.com>
2023-03-16 16:46:54 +0800

    后台 修改登录鉴权的方式，将数据存到数据库

---
+++ app/admin/controller/Base.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace app\admin\controller;
+
+use app\BaseController;
+use app\common\BaseServer;
+use app\model\AdminSession;
+use think\App;
+use think\facade\Request;
+
+class Base extends BaseController
+{
+    protected $baseServer;
+
+    public function __construct(App $app)
+    {
+        parent::__construct($app);
+        // 将BaseServer的响应数据挂载到类
+        $this->baseServer = new BaseServer();
+    }
+
+    protected function success($msg = "success",$data = [], $code = 0){
+       return $this->baseServer->success($msg ,$data, $code);
+    }
+    protected function error($msg = "fail",$data =[], $code = 1){
+        return $this->baseServer->error($msg,$data, $code);
+    }
+    protected function getError($error){
+        return $this->baseServer->getEorror($error);
+    }
+    protected function writeDoLog($data){
+        $this->baseServer->writeDoLog($data);
+    }
+
+
+    /**
+     * 重写基础控制器的初始化方法
+     * @return void
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\DbException
+     * @throws \think\db\exception\ModelNotFoundException
+     */
+    protected function initialize(){
+        $controller = $this->request->controller();
+        $action = $this->request->action();
+        $app = app('http')->getName();
+        $url = "{$app}/{$controller}/{$action}";
+        if (!in_array($url, config('my.nocheck'))) {
+            $this->checkTokenAuth();
+        }
+    }
+
+
+    /**
+     * 设置管理员登录的token
+     * @param $data
+     * @return string
+     */
+    public function setToken($data)
+    {
+
+        $token = md5(uniqid());//根据时间和md5生成唯一的token
+
+        //登录的时候把token写入数据表
+        $tokenInfo = AdminSession::where('token', $token)->find();
+        if (empty($tokenInfo)) {
+            AdminSession::create([
+                'token' => $token,
+                'expire_time' => time(),
+                'data' => json_encode($data),
+                'status' => 1
+            ]);
+        } else {
+            AdminSession::where('token', $token)->update([
+                'token' => $token,
+                'expire_time' => time(),
+                'data' => json_encode($data),
+                'status' => 1
+            ]);
+        }
+
+        return $token;
+    }
+
+    /**
+     * 获取token
+     * @return array|string
+     */
+    protected function getToken() {
+        return  Request::header('Authorization');
+    }
+
+
+    /**
+     * 判断token是否存在、判断登录状态是否应该存在
+     * @return \think\response\Json|void
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\DbException
+     * @throws \think\db\exception\ModelNotFoundException
+     */
+    protected function checkTokenAuth() {
+
+        $token = $this->getToken();
+
+        if(empty($token)) {
+            return $this->error('token不能为空，请确保处于登录状态！');
+        }
+
+        // 获取到指定的token信息
+        $tokenInfo = AdminSession::where('token','=', $token)->find();
+
+        if(empty($tokenInfo)) {
+            return $this->error('token不存在！登录信息错误！');
+        }
+
+        if($tokenInfo['status'] === 0) {
+            return $this->error('登录信息失效！请重新登录！');
+        }
+
+
+        // dump(config('my.token_expire_time'));
+        // dump(($tokenInfo['expire_time'] + config('my.token_expire_time')));
+        // dump(time());
+
+        if (($tokenInfo['expire_time'] + config('my.token_expire_time')) < time()) {
+            abort(101, '登录状态已过期，请重新登录');
+        }
+    }
+}
--- app/admin/controller/Login.php
+++ app/admin/controller/Login.php
@@ -5,17 +5,20 @@
 namespace app\admin\controller;
 
 use app\common\BaseServer;
+use app\model\AdminSession;
 use app\model\AdminUser;
 use app\Request;
 use app\validate\AdminUserValidate;
+use think\App;
 use think\Exception;
 use think\exception\ValidateException;
 use think\facade\Session;
 use think\Validate;
+use app\admin\controller\Base;
 
-class Login extends BaseServer
+class Login extends Base
 {
-    // 登录
+
     public function login(Request $request){
         $data = [];
         if(!empty($request->param('account'))){
@@ -34,21 +37,23 @@ class Login extends BaseServer
         if($res){
             // 验证密码是否正确
             if($res['password'] === md5($data['password'])){
-                Session::set('token',md5($data['account'].$data['password']));
-                $this->result = ['token'=>md5($data['account'].$data['password'])];
-                // 要保存的用户信息
-                $userInfo = [
-                    'title' => $res['title'],
-                    'avatar' => $res['avatar'],
-                    'account' => $res['account']
-                ];
-                // 将用户信息存到session中，用于获取管理员信息
-                Session::set('admin_info',$userInfo);
+                // Session::set('token',md5($data['account'].$data['password']));
+                // $this->result = ['token'=>md5($data['account'].$data['password'])];
+                // // 要保存的用户信息
+                // $userInfo = [
+                //     'title' => $res['title'],
+                //     'avatar' => $res['avatar'],
+                //     'account' => $res['account']
+                // ];
+                // // 将用户信息存到session中，用于获取管理员信息
+                // Session::set('admin_info',$userInfo);
+                //
+                // // 写入登录日志
+                // event('LoginLog', ['user' => $res['title']]);
 
-                // 写入登录日志
-                event('LoginLog', ['user' => $res['title']]);
-
-                return $this->success('登录成功！');
+                $token = $this->setToken($res); // 获取登录的token
+                $result = ['token'=>$token];
+                return $this->success('登录成功！',$result);
             }else{
                 return $this->error('密码错误！');
             }
@@ -57,25 +62,31 @@ class Login extends BaseServer
         }
     }
 
+
     // 获取登录用户的账号信息
     public function getInfo() {
-        $res = Session::get('admin_info');
-        $this->result = $res;
-        return $this->success();
+        // 获取token
+        $token = $this->getToken();
+
+        // 获取到登录用户的id,通过id管理员详情
+        $tokenInfo = AdminSession::where('token', $token)->find();
+        $id = json_decode($tokenInfo->data)->id;
+        $res = AdminUser::find($id);
+
+        return $this->success('success',$res);
     }
 
 
     // 退出登录
     public function logout(Request $request) {
-        $token = $request->param('token');
-        if(empty($token)){
-            return $this->error('请输入token！(请确保当前在登录状态)');
-        }
-        if($token !== Session::get('token')){
-            return $this->error('token输入错误！');
-        }
-        Session::delete('token');
-        Session::delete('admin_info');
+        // 获取token
+        $token = $this->getToken();
+        // 将登录token的状态status设置为0
+        $token = $this->getToken();
+        AdminSession::where('token', $token)->update([
+            'status' => 0
+        ]);
+
         return $this->success('退出成功!');
     }
 
@@ -108,9 +119,12 @@ class Login extends BaseServer
             $user->password = md5($newPassword);
             $bool = $user->save();
             if($bool) {
-                // 密码修改成功，删除token,前端手动退出登录
-                // Session::delete('token');
-                // Session::delete('admin_info');
+                // 将登录token的状态status设置为0
+                $token = $this->getToken();
+                AdminSession::where('token', $token)->update([
+                    'status' => 0
+                ]);
+
                 return $this->success('密码修改成功！');
             }else {
                 return $this->error('密码修改失败！');
