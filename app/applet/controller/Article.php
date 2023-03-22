80b0c50ab9b991b8be5280a163f50cf7852932d7
xyq <xyq135@gmail.com>
2023-03-22 20:35:06 +0800

    小程序 学习资源 根据分类分页获取文章列表

--- app/applet/controller/Article.php
+++ app/applet/controller/Article.php
@@ -36,4 +36,30 @@ class Article extends BaseServer
         $data = ['total'=>$total,'data'=>$res];
         return $this->success('success',$data);
     }
+
+    /**
+     * 根据分类获取文章分页列表
+     * @param Request $request
+     * @return \think\response\Json
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\DbException
+     * @throws \think\db\exception\ModelNotFoundException
+     */
+    public function page(Request $request) {
+        // 获取分类id
+        $id = $request->param('category_id');
+        $page = $request->param('page',1,'intval');
+        $limit = $request->param('limit',10,'intval');
+
+        if(empty($id)) {
+            return $this->error('请选择分类!');
+        }
+
+        $query = $this->article->where([['status','=',1],['study_category_id','=',$id]]);
+
+        $total = $query->count();
+        $res = $query->page($page,$limit)->select();
+        $data = ['total'=>$total,'data'=>$res];
+        return $this->success('success',$data);
+    }
 }
