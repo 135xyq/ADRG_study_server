1e75c132e6fa00c1756e7ab3704404261ca81cf8
xyq <xyq135@gmail.com>
2023-03-25 15:12:07 +0800

    后台  题库 题库分类的模型和数据验证

--- app/model/QuestionCategory.php
+++ app/model/QuestionCategory.php
@@ -8,4 +8,10 @@ use think\model\concern\SoftDelete;
 class QuestionCategory extends Model
 {
     use SoftDelete;
+
+    public function getStatusTextAttr($value,$data)
+    {
+        $status = [0=>'显示',1=>'不显示'];
+        return $status[$data['status']];
+    }
 }
