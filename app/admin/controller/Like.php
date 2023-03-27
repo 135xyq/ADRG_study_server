6ca2027fc24dbcc4108c64aed2ee73b472f31348
xyq <xyq135@gmail.com>
2023-03-27 22:00:03 +0800

    后台 点赞 增加用户名筛选列表

--- app/admin/controller/Like.php
+++ app/admin/controller/Like.php
@@ -2,6 +2,7 @@
 
 namespace app\admin\controller;
 
+use app\model\AppletUser;
 use app\Request;
 use app\validate\LikeValidate;
 use think\App;
@@ -32,6 +33,8 @@ class Like extends Base
         $limit = $request->param('limit', 20, 'intval');
         $article = $request->param('article', ''); // 文章id
         $video = $request->param('video', ''); // 视频id
+        $userName = $request->param('userName',''); // 用户名
+
 
         // 查询条件
         $where = [];
@@ -51,6 +54,13 @@ class Like extends Base
         },'user' => function($query) {
             $query->field('id,nick_name');
         }])->where($where);
+
+
+        $where = AppletUser::where('nick_name','like','%'.$userName.'%');
+
+        if($userName !== '') {
+            $query->hasWhere('user',$where);
+        }
 
 
         $total = $query->count(); // 统计数量
