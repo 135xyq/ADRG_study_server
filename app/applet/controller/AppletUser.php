60ec90c1b628716e5efbdae3a06259a7c2fc4a18
xyq <xyq135@gmail.com>
2023-03-28 16:06:05 +0800

    小程序 个人中心  用户详情

---
+++ app/applet/controller/AppletUser.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace app\applet\controller;
+
+use app\Request;
+use think\facade\Cache;
+
+class AppletUser extends Base
+{
+    public function getUserInfo(Request $request) {
+        $userInfo = Cache::get($this->token);
+        return $this->success('success',$userInfo);
+    }
+}
---
+++ app/applet/controller/Base.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace app\applet\controller;
+
+use app\BaseController;
+use app\common\BaseServer;
+use think\App;
+use think\facade\Cache;
+use think\facade\Request;
+
+class Base extends BaseController
+{
+    protected $baseServer;
+    protected $token;
+
+    public function __construct(App $app)
+    {
+        parent::__construct($app);
+        // 将BaseServer的响应数据挂载到类
+        $this->baseServer = new BaseServer();
+    }
+
+    protected function success($msg = "success",$data = [], $code = 0){
+        return $this->baseServer->success($msg ,$data, $code);
+    }
+    protected function error($msg = "fail",$data =[], $code = 1){
+        return $this->baseServer->error($msg,$data, $code);
+    }
+    protected function getError($error){
+        return $this->baseServer->getEorror($error);
+    }
+
+    /**
+     * 重写基础控制器的初始化方法
+     * @return void
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\DbException
+     * @throws \think\db\exception\ModelNotFoundException
+     */
+    protected function initialize(){
+        $this->checkTokenAuth();
+    }
+
+    /**
+     * 校验是否登录，有token
+     */
+    protected function checkTokenAuth() {
+
+        $token = Request::param('token');
+
+        if(empty($token)) {
+            abort(403,'token不存在');
+        }
+
+        $userInfo = Cache::get($token);
+        if(empty($userInfo)){
+            abort(403,'token不存在');
+        }
+
+        $this->token = $token;
+    }
+}
