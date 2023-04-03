e7a105ba9c6cc234898481f706ba73ec1bec8945
xyq <xyq135@gmail.com>
2023-04-03 14:16:04 +0800

    小程序 评论 用户评论

--- app/applet/controller/Comment.php
+++ app/applet/controller/Comment.php
@@ -3,19 +3,31 @@
 namespace app\applet\controller;
 
 use app\Request;
+use app\validate\CommentValidate;
 use think\App;
 use app\model\Comment as CommentModel;
+use think\exception\ValidateException;
 
 class Comment extends Base
 {
 
     protected $comment;
+    protected $userId;
     public function __construct(App $app)
     {
         parent::__construct($app);
+        $this->userId = $this->userInfo['id'];
         $this->comment = new CommentModel();
     }
 
+    /**
+     * 分页获取评论
+     * @param Request $request
+     * @return \think\response\Json
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\DbException
+     * @throws \think\db\exception\ModelNotFoundException
+     */
     public function page(Request $request) {
         $page = $request->param('page', 1, 'intval');
         $limit = $request->param('limit', 20, 'intval');
@@ -49,5 +61,44 @@ class Comment extends Base
         ];
 
         return $this->success('success',$data);
+    }
+
+    /**
+     * 用户评论
+     * @param Request $request
+     * @return \think\response\Json
+     */
+    public function publishComment(Request $request) {
+        $content = $request->param('content');
+        $videoId = $request->param('videoId');
+        $articleId = $request->param('articleId');
+
+        // 设置用户信息和默认值
+        $applet_user_id = $this->userId;
+        $status = 0;
+
+        if(empty($articleId) && empty($videoId)) {
+            return $this->error('请选择评论的对象！');
+        }
+
+        try {
+            validate(CommentValidate::class)->scene('add')->check([
+                'content' => $content,
+                'applet_user_id' => $applet_user_id,
+                'status' => $status
+            ]);
+        }catch (ValidateException $e){
+            return $this->error('评论内容不能为空！');
+        }
+
+        $res = CommentModel::create([
+            'content' => $content,
+            'applet_user_id' => $applet_user_id,
+            'video_id' => $videoId,
+            'article_id' => $articleId,
+            'status' => $status
+        ]);
+
+        return $this->success('评论成功');
     }
 }
