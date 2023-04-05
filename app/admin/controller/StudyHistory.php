35e8e9acbd1d17a98d4f6d4705aafc58d365ace3
xyq <xyq135@gmail.com>
2023-04-05 16:27:30 +0800

    后台 学习记录 分页获取

---
+++ app/admin/controller/StudyHistory.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace app\admin\controller;
+use app\model\StudyHistory as StudyHistoryModel;
+use app\Request;
+use think\App;
+use app\model\AppletUser;
+
+class StudyHistory extends Base
+{
+
+    protected $studyHistory;
+
+    public function __construct(App $app)
+    {
+        parent::__construct($app);
+        $this->studyHistory = new StudyHistoryModel();
+    }
+
+    /**
+     * 分页查询用户的观看列表
+     * @param Request $request
+     * @return \think\response\Json
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\DbException
+     * @throws \think\db\exception\ModelNotFoundException
+     */
+    public function page(Request $request)
+    {
+        $page = $request->param('page', 1, 'intval');
+        $limit = $request->param('limit', 20, 'intval');
+        $article = $request->param('article', ''); // 文章id
+        $video = $request->param('video', ''); // 视频id
+        $userName = $request->param('userName',''); // 用户名
+        $type = $request->param('type',''); // 种类
+
+        // 查询条件
+        $where = [];
+        if (!empty($article)) {
+            $where['article_id'] = $article;
+        }
+
+        if (!empty($video)) {
+            $where['video_id'] = $video;
+        }
+
+        if (!empty($type)) {
+            $where['type'] = $type;
+        }
+
+        // 用户名检索
+        $where1 = AppletUser::where('nick_name','like','%'.$userName.'%');
+
+        // 筛选基本条件
+        $query = $this->studyHistory->with(['video' => function ($query) {
+            $query->field('id,title');
+        }, 'article' => function ($query) {
+            $query->field('id,title');
+        },'user' => function($query) {
+            $query->field('id,nick_name');
+        }])->hasWhere('user',$where1)->where($where);
+
+
+        $total = $query->count(); // 统计数量
+        $res = $query->page($page, $limit)->select();
+
+        $data = [
+            'total' => $total,
+            'data' => $res
+        ];
+
+        return $this->success('success', $data);
+    }}
