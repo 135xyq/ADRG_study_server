0b202d7e025357947ffc7ed836cc3d19e0999808
xyq <xyq135@gmail.com>
2023-04-05 16:39:41 +0800

    后台 学习记录 批量删除

--- app/admin/controller/StudyHistory.php
+++ app/admin/controller/StudyHistory.php
@@ -1,10 +1,13 @@
 <?php
 
 namespace app\admin\controller;
+
 use app\model\StudyHistory as StudyHistoryModel;
 use app\Request;
+use app\validate\StudyHistoryValidate;
 use think\App;
 use app\model\AppletUser;
+use think\exception\ValidateException;
 
 class StudyHistory extends Base
 {
@@ -31,8 +34,8 @@ class StudyHistory extends Base
         $limit = $request->param('limit', 20, 'intval');
         $article = $request->param('article', ''); // 文章id
         $video = $request->param('video', ''); // 视频id
-        $userName = $request->param('userName',''); // 用户名
-        $type = $request->param('type',''); // 种类
+        $userName = $request->param('userName', ''); // 用户名
+        $type = $request->param('type', ''); // 种类
 
         // 查询条件
         $where = [];
@@ -49,16 +52,16 @@ class StudyHistory extends Base
         }
 
         // 用户名检索
-        $where1 = AppletUser::where('nick_name','like','%'.$userName.'%');
+        $where1 = AppletUser::where('nick_name', 'like', '%' . $userName . '%');
 
         // 筛选基本条件
         $query = $this->studyHistory->with(['video' => function ($query) {
             $query->field('id,title');
         }, 'article' => function ($query) {
             $query->field('id,title');
-        },'user' => function($query) {
+        }, 'user' => function ($query) {
             $query->field('id,nick_name');
-        }])->hasWhere('user',$where1)->where($where);
+        }])->hasWhere('user', $where1)->where($where);
 
 
         $total = $query->count(); // 统计数量
@@ -70,4 +73,36 @@ class StudyHistory extends Base
         ];
 
         return $this->success('success', $data);
-    }}
+    }
+
+    /**
+     * 删除学习记录，支持批量删除
+     * @param Request $request
+     * @return \think\response\Json
+     */
+    public function delete(Request $request)
+    {
+        $id = $request->param('id');
+
+        try {
+            validate(StudyHistoryValidate::class)->scene('delete')->check(
+                ['id' => $id]
+            );
+        } catch (ValidateException $e) {
+            return $this->error($e->getError());
+        }
+
+        $ids = ['id' => explode(',', $id)];
+        $bool = StudyHistoryModel::destroy($ids);
+        if ($bool) {
+
+            // 写入操作日志
+            $this->writeDoLog($request->param());
+
+
+            return $this->success('删除成功！');
+        } else {
+            return $this->error('删除失败');
+        }
+    }
+}
