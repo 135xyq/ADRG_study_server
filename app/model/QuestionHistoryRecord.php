2eb1d8933941b76b0ed5fef6f09bf1d8de3619d3
xyq <xyq135@gmail.com>
2023-04-06 16:40:34 +0800

    做题记录模型 新增方法：获取错题和没做过的题

--- app/model/QuestionHistoryRecord.php
+++ app/model/QuestionHistoryRecord.php
@@ -16,12 +16,51 @@ class QuestionHistoryRecord extends Model
 
     // 关联出题记录表
     public function questionRecord() {
-        $this->belongsTo(QuestionRecord::class);
+        return $this->belongsTo(QuestionRecord::class);
     }
 
     // 关联题目表
     public function question() {
-        $this->belongsTo(Question::class);
+        return $this->belongsTo(Question::class);
+    }
+
+    /**
+     * 获取错题列表
+     * @param $user
+     * @return array|\think\Collection|\think\db\BaseQuery[]
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\DbException
+     * @throws \think\db\exception\ModelNotFoundException
+     */
+    public function getErrorQuestion($user) {
+        // 获取记录表中的那些已经提交的错题
+        $where = QuestionRecord::where('applet_user_id','=',$user)->where('is_submit','=','1');
+
+        $data = $this->hasWhere('questionRecord',$where)->where('is_current','=',0)->select();
+
+        return $data;
+    }
+
+
+    /**
+     * 获取没有做过的新题
+     * @param $user
+     * @return Question[]|array|\think\Collection
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\DbException
+     * @throws \think\db\exception\ModelNotFoundException
+     */
+    public function getNewQuestion($user) {
+        // 获取记录表中的那些已经提交
+        $where = QuestionRecord::where('applet_user_id','=',$user)->where('is_submit','=','1');
+
+        // 获取用户已经做过的题目列表
+        $doneIds = $this->hasWhere('questionRecord',$where)->column('question_id');
+
+        // 获取没有做过的题目
+        $data = Question::whereNotIn('id',$doneIds)->select();
+
+        return $data;
     }
 
 }
