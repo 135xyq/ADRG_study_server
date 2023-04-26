9b453aa5853d309a16888ab3b3637979ef31e1ab
xyq <xyq135@gmail.com>
2023-04-26 18:17:53 +0800

    后台 小程序配置 加入评论和昵称的审核、敏感词

--- app/admin/controller/AppletConfig.php
+++ app/admin/controller/AppletConfig.php
@@ -17,72 +17,115 @@ class AppletConfig extends Base
 {
 
     protected $appletConfigModel;
-    public function __construct(App $app){
+
+    public function __construct(App $app)
+    {
         parent::__construct($app);
         $this->appletConfigModel = new AppletConfigModel();
     }
-        //获取配置信息
-    public function get(){
+
+    //获取配置信息
+    public function get()
+    {
         $config = AppletConfigModel::select();
         // 存在配置
-        if(count($config) >0){
+        if (count($config) > 0) {
             return $this->success('success', $config[0]);
-        }else{
-            return $this->success('t');
+        } else {
+            return $this->success('success');
         }
     }
 
-     // 修改小程序配置信息
-     public function update(Request $request){
-         $id = $request->param('id');
-         // 验证数据合理性
-         try {
-             validate(AppletConfigValidate::class)->scene('update')->check([
-                 'id' => $id
-             ]);
-         } catch (ValidateException $e) {
-             return $this->error($e->getError());
-         }
-         if(empty($this->appletConfigModel->where('id',$id)->find())){
-             return $this->error('id传入错误！要修改的配置不存在！');
-         }
-         if(!empty($request->param('appid'))){
-             $saveData['appid'] = $request->param('appid');
-         }
-         if(!empty($request->param('secret'))){
-             $saveData['secret'] = $request->param('secret');
-         }
-         $bool = $this->appletConfigModel->where('id',$id)->update($saveData);
-         if($bool){
-             // 写入操作日志
-             $this->writeDoLog($request->param());
+    /**
+     * 修改小程序的配置信息
+     * @param Request $request
+     * @return \think\response\Json
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\DbException
+     * @throws \think\db\exception\ModelNotFoundException
+     */
+    public function update(Request $request)
+    {
+        $id = $request->param('id');
+        $is_auto_check_comment = $request->param('is_auto_check_comment', '');
+        $is_auto_check_user_name = $request->param('is_auto_check_user_name', '');
+        $sensitive_words = $request->param('sensitive_words', []);
 
-             return $this->success('修改成功！');
-         }else{
-             return $this->error('修改失败');
-         }
-     }
+        // 验证数据合理性
+        try {
+            validate(AppletConfigValidate::class)->scene('update')->check([
+                'id' => $id,
+                'is_auto_check_comment' => $is_auto_check_comment,
+                'is_auto_check_user_name' => $is_auto_check_user_name
+            ]);
+        } catch (ValidateException $e) {
+            return $this->error($e->getError());
+        }
 
-    // 新增小程序配置
-    public function add(Request $request){
+        if (empty($this->appletConfigModel->where('id', $id)->find())) {
+            return $this->error('id传入错误！要修改的配置不存在！');
+        }
+
+        if (!empty($request->param('appid'))) {
+            $saveData['appid'] = $request->param('appid');
+        }
+
+        if (!empty($request->param('secret'))) {
+            $saveData['secret'] = $request->param('secret');
+        }
+
+        $saveData['is_auto_check_comment'] = $is_auto_check_comment;
+        $saveData['is_auto_check_user_name'] = $is_auto_check_user_name;
+        $saveData['sensitive_words'] = $sensitive_words;
+
+        // 修改数据
+        $config = $this->appletConfigModel->find($id);
+        $bool = $config->save($saveData);
+
+        if ($bool) {
+            // 写入操作日志
+            $this->writeDoLog($request->param());
+
+            return $this->success('修改成功！');
+        } else {
+            return $this->error('修改失败');
+        }
+    }
+
+    /**
+     * 新增小程序配置
+     * @param Request $request
+     * @return \think\response\Json
+     */
+    public function add(Request $request)
+    {
         $appid = $request->param('appid');
         $secret = $request->param('secret');
+        $is_auto_check_comment = $request->param('is_auto_check_comment', 0);
+        $is_auto_check_user_name = $request->param('is_auto_check_user_name', 0);
+        $sensitive_words = $request->param('sensitive_words', []);
+
         // 验证数据合理性
         try {
             validate(AppletConfigValidate::class)->scene('add')->check([
                 'appid' => $appid,
-                'secret' => $secret
+                'secret' => $secret,
+                'is_auto_check_comment' => $is_auto_check_comment,
+                'is_auto_check_user_name' => $is_auto_check_user_name
             ]);
         } catch (ValidateException $e) {
             return $this->error($e->getError());
         }
         $bool = $this->appletConfigModel->save([
             'appid' => $appid,
-            'secret' => $secret
+            'secret' => $secret,
+            "is_auto_check_comment" => $is_auto_check_comment,
+            "is_auto_check_user_name" => $is_auto_check_user_name,
+            "sensitive_words" => $sensitive_words
         ]);
-        if($bool){
+        if ($bool) {
             return $this->success('新增成功！');
-        }else{
+        } else {
             return $this->error('新增失败!');
         }
     }
