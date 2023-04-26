e348d8d99a2f0e280f0fedca67ead33ece500e42
xyq <xyq135@gmail.com>
2023-04-26 14:30:05 +0800

    后台 首页的数据统计 加入用户反馈的统计

--- app/admin/controller/Dashboard.php
+++ app/admin/controller/Dashboard.php
@@ -46,23 +46,24 @@ class Dashboard extends Base
     public function index()
     {
 
-        // 统计用户、视频、文章、题目的个数
+        // 统计用户、视频、文章、题目、用户反馈的个数
         $statistical['userCount'] = $this->user->count();
         $statistical['videoCount'] = $this->video->count();
         $statistical['articleCount'] = $this->article->count();
         $statistical['questionCount'] = $this->question->count();
+        $statistical['feedbackCount'] = $this->feedback->count();
 
         // 统计最近10天新增的评论、点赞、收藏、注册用户、用户反馈
-        $newAddCount['comment'] = $this->getLastTenDayCount($this->comment,10);
-        $newAddCount['like'] = $this->getLastTenDayCount($this->like,10);
-        $newAddCount['star'] = $this->getLastTenDayCount($this->star,10);
-        $newAddCount['user'] = $this->getLastTenDayCount($this->user,10);
-        $newAddCount['feedback'] = $this->getLastTenDayCount($this->feedback,10);
+        $newAddCount['comment'] = $this->getLastTenDayCount($this->comment, 10);
+        $newAddCount['like'] = $this->getLastTenDayCount($this->like, 10);
+        $newAddCount['star'] = $this->getLastTenDayCount($this->star, 10);
+        $newAddCount['user'] = $this->getLastTenDayCount($this->user, 10);
+        $newAddCount['feedback'] = $this->getLastTenDayCount($this->feedback, 10);
 
         // 统计排名前十的文章、视频、题目作答情况
-        $topCount['article'] = $this->article->order('view_count','desc')->limit(10)->field(['id','title','view_count'])->select();
-        $topCount['video'] = $this->video->order('view_count','desc')->limit(10)->field(['id','title','view_count'])->select();
-        $topCount['question'] = $this->question->order('test_count','desc')->limit(10)->field(['id','title','test_count'])->select();
+        $topCount['article'] = $this->article->order('view_count', 'desc')->limit(10)->field(['id', 'title', 'view_count'])->select();
+        $topCount['video'] = $this->video->order('view_count', 'desc')->limit(10)->field(['id', 'title', 'view_count'])->select();
+        $topCount['question'] = $this->question->order('test_count', 'desc')->limit(10)->field(['id', 'title', 'test_count'])->select();
 
         $data = [
             'statistical' => $statistical,
@@ -70,7 +71,7 @@ class Dashboard extends Base
             'topCount' => $topCount
         ];
 
-        return $this->success('success',$data);
+        return $this->success('success', $data);
     }
 
     /**
@@ -88,7 +89,7 @@ class Dashboard extends Base
             // 结束时间
             $endDate = date("Y-m-d H:i:s", strtotime("-" . ($dayCount - $i - 1) . "day"));
 
-            $count[] = $query->whereBetweenTime('create_time',$startDate,$endDate)->count();
+            $count[] = $query->whereBetweenTime('create_time', $startDate, $endDate)->count();
         }
 
         return $count;
