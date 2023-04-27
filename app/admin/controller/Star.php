7092a323b7ebac28b506cdd405da96d50056c4a1
xyq <xyq135@gmail.com>
2023-04-27 21:08:06 +0800

    后台 收藏 获取收藏的分页列表 增加按照时间倒序

--- app/admin/controller/Star.php
+++ app/admin/controller/Star.php
@@ -59,7 +59,7 @@ class Star extends Base
 
 
         $total = $query->count(); // 统计数量
-        $res = $query->page($page, $limit)->select();
+        $res = $query->order('create_time','desc')->page($page, $limit)->select();
 
         $data = [
             'total' => $total,
