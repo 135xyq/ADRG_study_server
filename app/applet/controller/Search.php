7fb70be2287325320ee63cc46f792dfffb3d7eb5
xyq <xyq135@gmail.com>
2023-05-01 23:25:28 +0800

    小程序 关键词搜索

---
+++ app/applet/controller/Search.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace app\applet\controller;
+
+use app\Request;
+use think\App;
+
+use app\model\Article as ArticleModel;
+use app\model\Video as VideoModel;
+use app\model\Question as QuestionModel;
+
+class Search extends Base
+{
+    protected $article;
+    protected $video;
+    protected $question;
+    public function __construct(App $app)
+    {
+        parent::__construct($app);
+        $this->question = new QuestionModel();
+        $this->article = new ArticleModel();
+        $this->video = new VideoModel();
+    }
+
+    /**
+     * 根据关键词查询
+     * @param Request $request
+     * @return \think\response\Json
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\DbException
+     * @throws \think\db\exception\ModelNotFoundException
+     */
+    public function index(Request $request) {
+        // 查询的分类
+        $type = $request->param('type');
+        // 要查询的内容
+        $content = $request->param('content');
+        $page = $request->param('page',1,'intval');
+        $limit = $request->param('limit',20,'intval');
+
+        $query = null;
+
+        // 查询文章
+        if($type === 'article') {
+            $query = $this->article->where('title','like','%'.$content.'%');
+        }
+
+        // 查询视频
+        if($type === 'video') {
+            $query = $this->video->where('title','like','%'.$content.'%');
+        }
+
+        // 查询文章
+        if($type === 'question') {
+            $query = $this->question->where('title','like','%'.$content.'%');
+        }
+
+        $total = $query->count();
+        $res = $query->field(['id','title'])->page($page,$limit)->select();
+
+        $data = [
+            'total' => $total,
+            'data' => $res
+        ];
+
+        return $this->success('success',$data);
+    }
+}
